6a3ddb37ec650f9135b328047f52c71a2ceb4698
Caroline Jarman <carolinerosejarman@gmail.com>
2024-05-06 16:45:46 -0600

    adds Knight MC, all tests pass

--- shared/src/main/java/chess/pieces/KnightMovesCalculator.java
+++ shared/src/main/java/chess/pieces/KnightMovesCalculator.java
@@ -1,15 +1,46 @@
 package chess.pieces;
 
-import chess.ChessBoard;
-import chess.ChessMove;
-import chess.ChessPosition;
+import chess.*;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class KnightMovesCalculator implements PieceMovesCalculator{
   @Override
   public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-    return List.of();
+
+    ChessPiece currentPiece = board.getPiece(myPosition);
+    int startingRow = myPosition.getRow();
+    int startingCol = myPosition.getColumn();
+    ChessGame.TeamColor currentColor = currentPiece.getTeamColor();
+    Collection<ChessMove> possMoves = new ArrayList<>();
+
+    Collection<ChessMove> allKnightMoves = new ArrayList<>();
+    allKnightMoves.add(new ChessMove(myPosition, new ChessPosition(startingRow+2, startingCol+1), null));
+    allKnightMoves.add(new ChessMove(myPosition, new ChessPosition(startingRow+2, startingCol-1), null));
+    allKnightMoves.add(new ChessMove(myPosition, new ChessPosition(startingRow+1, startingCol+2), null));
+    allKnightMoves.add(new ChessMove(myPosition, new ChessPosition(startingRow+1, startingCol-2), null));
+    allKnightMoves.add(new ChessMove(myPosition, new ChessPosition(startingRow-2, startingCol+1), null));
+    allKnightMoves.add(new ChessMove(myPosition, new ChessPosition(startingRow-2, startingCol-1), null));
+    allKnightMoves.add(new ChessMove(myPosition, new ChessPosition(startingRow-1, startingCol+2), null));
+    allKnightMoves.add(new ChessMove(myPosition, new ChessPosition(startingRow-1, startingCol-2), null));
+
+
+    for(ChessMove move: allKnightMoves) {
+      if(move.getEndPosition().checkValidPosition()){ //valid move (on the board)
+        //now check if there is already a piece there
+        if(board.getPiece(move.getEndPosition()) == null){ // valid and empty
+          possMoves.add(new ChessMove(myPosition, move.getEndPosition(), null));
+
+        } else if(board.getPiece(move.getEndPosition()) != null){
+          // valid spot but a piece IS there, now check color of the piece
+          if(currentColor != board.getPiece(move.getEndPosition()).getTeamColor()){ // different teams
+            possMoves.add(new ChessMove(myPosition, move.getEndPosition(), null));
+          }
+        } //remove the BREAK
+      }
+    }
+    return possMoves;
   }
 }
