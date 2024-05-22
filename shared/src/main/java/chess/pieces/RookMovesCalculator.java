e1800c58a9e5567826ec03a478ff03cc3a81c21d
Caroline Jarman <carolinerosejarman@gmail.com>
2024-05-21 20:49:14 -0600

    removes some old comments

--- shared/src/main/java/chess/pieces/RookMovesCalculator.java
+++ shared/src/main/java/chess/pieces/RookMovesCalculator.java
@@ -16,79 +16,7 @@ public class RookMovesCalculator implements PieceMovesCalculator{
     possMoves.addAll(getRookMovesVertical(board, myPosition, 1));
     possMoves.addAll(getRookMovesVertical(board, myPosition, -1));
     return possMoves;
-
-//    return getRookMoves(board, myPosition);
   }
-
-//
-//  public Collection<ChessMove> getRookMoves(ChessBoard board, ChessPosition myPosition){
-//    ChessPiece currentPiece = board.getPiece(myPosition);
-//    int startingRow = myPosition.getRow();
-//    int startingCol = myPosition.getColumn();
-//    ChessGame.TeamColor currentColor = currentPiece.getTeamColor();
-//    Collection<ChessMove> possMoves = new ArrayList<>();
-//
-//    ChessPosition newPosition;
-//
-//    // check going right
-//    newPosition = new ChessPosition(startingRow, startingCol+1);
-//    while(newPosition.checkValidPosition()){
-//      if(board.getPiece(newPosition) == null){
-//        possMoves.add(new ChessMove(myPosition, newPosition, null));
-//      } else if(board.getPiece(newPosition) != null){ //when we run into a piece
-//        if(currentColor != board.getPiece(newPosition).getTeamColor()){
-//          possMoves.add(new ChessMove(myPosition, newPosition, null));
-//        }
-//        break;
-//      }
-//      newPosition = new ChessPosition(startingRow, newPosition.getColumn() + 1);
-//    }
-//
-//    // check going left
-//    newPosition = new ChessPosition(startingRow, startingCol-1);
-//    while(newPosition.checkValidPosition()){
-//      if(board.getPiece(newPosition) == null){
-//        possMoves.add(new ChessMove(myPosition, newPosition, null));
-//      } else if(board.getPiece(newPosition) != null){ //when we run into a piece
-//        if(currentColor != board.getPiece(newPosition).getTeamColor()){
-//          possMoves.add(new ChessMove(myPosition, newPosition, null));
-//        }
-//        break;
-//      }
-//      newPosition = new ChessPosition(startingRow, newPosition.getColumn() - 1);
-//    }
-//
-//    // check going down
-//    newPosition = new ChessPosition(startingRow -1, startingCol);
-//    while(newPosition.checkValidPosition()){
-//      if(board.getPiece(newPosition) == null){
-//        possMoves.add(new ChessMove(myPosition, newPosition, null));
-//      } else if(board.getPiece(newPosition) != null){ //when we run into a piece
-//        if(currentColor != board.getPiece(newPosition).getTeamColor()){
-//          possMoves.add(new ChessMove(myPosition, newPosition, null));
-//        }
-//        break;
-//      }
-//      newPosition = new ChessPosition(newPosition.getRow() -1, startingCol);
-//    }
-//
-//    // check going up
-//    newPosition = new ChessPosition(startingRow +1, startingCol);
-//    while(newPosition.checkValidPosition()){
-//      if(board.getPiece(newPosition) == null){
-//        possMoves.add(new ChessMove(myPosition, newPosition, null));
-//      } else if(board.getPiece(newPosition) != null){ //when we run into a piece
-//        if(currentColor != board.getPiece(newPosition).getTeamColor()){
-//          possMoves.add(new ChessMove(myPosition, newPosition, null));
-//        }
-//        break;
-//      }
-//      newPosition = new ChessPosition(newPosition.getRow() +1, startingCol);
-//    }
-//
-//    return possMoves;
-//  }
-//
 
   public Collection<ChessMove> getRookMovesHorizontal(ChessBoard board, ChessPosition myPosition, int colAdvance){
     ChessPiece currentPiece = board.getPiece(myPosition);
@@ -112,11 +40,6 @@ public class RookMovesCalculator implements PieceMovesCalculator{
     }
     return possMoves;
   }
-
-
-
-
-
 
   public Collection<ChessMove> getRookMovesVertical(ChessBoard board, ChessPosition myPosition, int rowAdvance){
     ChessPiece currentPiece = board.getPiece(myPosition);
