3d35218a08c7df3b24620720d2de25de43e78c32
Caroline Jarman <carolinerosejarman@gmail.com>
2024-06-04 21:20:35 -0600

    adds main draw method and draw headers

--- client/src/main/java/ui/DrawBoard.java
+++ client/src/main/java/ui/DrawBoard.java
@@ -24,4 +24,63 @@ public class DrawBoard {
   }
 
 
+  private void draw(){
+    var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
+    out.print(ERASE_SCREEN);
+
+    // for WHITE perspective
+    out.print(SET_BG_COLOR_BLACK);
+    out.print(SET_TEXT_COLOR_LIGHT_GREY);
+    out.println("          WHITE PERSPECTIVE");
+
+    drawHeaders(out);
+    drawChessBoard(out);
+    drawHeaders(out);
+    out.print(SET_BG_COLOR_BLACK);
+    out.print(SET_TEXT_COLOR_WHITE);
+
+    out.println(); //empty line between the 2 boards
+
+    // for BLACK perspective ...
+    out.print(SET_TEXT_COLOR_LIGHT_GREY);
+    out.println("          BLACK PERSPECTIVE");
+    drawHeadersBack(out);
+    drawChessBoardBack(out);
+    drawHeadersBack(out);
+
+    out.println();
+    out.print(RESET_TEXT_COLOR);
+    out.print(RESET_BG_COLOR);
+  }
+
+  private void drawHeaders(PrintStream out) {
+    printOneBlackSquare(out);
+    setGrey(out);
+    String[] headers = { "  ", "a", "b", "c", "d", "e", "f", "g", "h", "  "};
+    for (int boardCol = 0; boardCol < 10; ++boardCol) {
+      setGrey(out);
+      out.print(headers[boardCol]);
+      if (boardCol < 9) {
+        out.print(EMPTY.repeat(2));
+      }
+    }
+    out.print(RESET_BG_COLOR);
+    out.println();
+  }
+  private void drawHeadersBack(PrintStream out) {
+    printOneBlackSquare(out);
+    setGrey(out);
+    String[] headers = { "  ", "h", "g", "f", "e", "d", "c", "b", "a", "  "};
+    for (int boardCol = 0; boardCol < 10; ++boardCol) {
+      setGrey(out);
+      out.print(headers[boardCol]);
+      if (boardCol < 9) {
+        out.print(EMPTY.repeat(2));
+      }
+    }
+    out.print(RESET_BG_COLOR);
+    out.println();
+  }
+
 }
