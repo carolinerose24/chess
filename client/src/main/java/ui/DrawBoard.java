657cea411ca00951917d720334a23a7f1419ba39
Caroline Jarman <carolinerosejarman@gmail.com>
2024-06-04 21:19:56 -0600

    adds private data members and constructors

--- client/src/main/java/ui/DrawBoard.java
+++ client/src/main/java/ui/DrawBoard.java
@@ -2,29 +2,26 @@ package ui;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static ui.EscapeSequences.*;
 
 public class DrawBoard {
 
+  private final String EMPTY = " ";
+  private ArrayList<ArrayList<String>> board = new ArrayList<>(8);
 
 
-  // and use a lot of ones form escape sequence
-
-  private String [][] squares = new String[8][8];
-  // just have it be Strings like BLACK_KING??
-  public DrawBoard(String[][] squares){ // with parameters, pass in the 2D array, do this more later??
-    this.squares = squares;
+  public DrawBoard(ArrayList<ArrayList<String>> board){ // with parameters, pass in the 2D array, do this more later??
+    this.board = board;
+    draw();
   }
 
   public DrawBoard(){ // no parameters, set squares equal to a default board
-
-
-
-
+    setDefaultBoard();
+    draw();
   }
-
-
 
 
 }
